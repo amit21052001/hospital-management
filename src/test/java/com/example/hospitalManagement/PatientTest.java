ca75b4a2224a96ca636a0f3b8a6ca87dfdcd22de
amit21052001 <amitsolankipqrs@gmail.com>
2026-01-14 01:40:02 +0530

    Added Services, Repositories And Tests Classes

---
+++ src/test/java/com/example/hospitalManagement/PatientTest.java
@@ -0,0 +1,41 @@
+package com.example.hospitalManagement;
+
+import com.example.hospitalManagement.Entity.Patient;
+import com.example.hospitalManagement.Repository.PatientRepo;
+import com.example.hospitalManagement.Service.PatientService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@SpringBootTest
+public class PatientTest {
+
+    @Autowired
+    private PatientRepo patientRepo;
+
+    @Autowired
+    private PatientService patientService;
+
+    @Test
+    public void testPatientRepository() {
+        Patient patient = new Patient();
+        patient.setBirthDate(LocalDate.of(2026, 1, 1));
+        patient.setGender("Male");
+        patient.setName("Amit");
+        patient.setEmail("Test#gmail.com");
+
+        patientRepo.save(patient);
+
+        List<Patient> patients = patientRepo.findAll();
+        System.out.println(patients);
+    }
+
+    @Test
+    public void testTransactionalMethods() {
+        Patient p1 = patientService.getPatientById(1L);
+        System.out.println(p1);
+    }
+}
