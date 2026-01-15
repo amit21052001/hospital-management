6361b63349191a36d89dc5a93dae79d9c0146caa
Amit Solanki <79105647+amit21052001@users.noreply.github.com>
2026-01-15 15:30:45 +0530

    Feature/branch-1 : Updating readme file, Repo methods, And Added Data.sql file. (#2)
    * Small Change Added
    
    * Small Change Added
    
    * Properties of @Column Annotation
    
    * Updating readme file, Repo methods, And Added Data.sql file.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # hospital-management
 Spring Boot Application for Hospital Management
+
+[![Java](https://img.shields.io/badge/Java-25-orange?logo=java)](https://www.oracle.com/java/)
+[![Spring Boot](https://img.shields.io/badge/Spring%20Boot-3.2.0-brightgreen?logo=springboot)](https://spring.io/projects/spring-boot)
+[![Spring Data JPA](https://img.shields.io/badge/Spring%20Data%20JPA-Enabled-success?logo=spring)](https://spring.io/projects/spring-data-jpa)
--- src/main/java/com/example/hospitalManagement/Entity/Patient.java
+++ src/main/java/com/example/hospitalManagement/Entity/Patient.java
@@ -1,7 +1,9 @@
 package com.example.hospitalManagement.Entity;
 
+import com.example.hospitalManagement.Enum.BloodGroupType;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.boot.web.servlet.ServletRegistration;
 
 import java.time.LocalDate;
@@ -12,25 +14,37 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-@Table(
-        name = "Patient_Table",
-        uniqueConstraints = {
-                @UniqueConstraint(name = "email_unique_constraint", columnNames = {"email"}),
-                @UniqueConstraint(name = "unique_patient_name_birthdate", columnNames = {"name", "birthDate"})
-        },
-        indexes = {
-                @Index(name = "idx_patient_birth_date", columnList = "birthDate")
-        }
-)
+//@Table(
+//        name = "Patient_Table",
+//        uniqueConstraints = {
+//                @UniqueConstraint(name = "email_unique_constraint", columnNames = {"email"}),
+//                @UniqueConstraint(name = "unique_patient_name_birthdate", columnNames = {"name", "birthDate"})
+//        },
+//        indexes = {
+//                @Index(name = "idx_patient_birth_date", columnList = "birthDate")
+//        }
+//)
 public class Patient {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+//    @Column(name = "FIRST_NAME", nullable = false, length = 40)
     private String name;
 
     @ToString.Exclude
     private LocalDate birthDate;
+
+    @Column(nullable = false)
     private String email;
+
     private String gender;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDate createdAt;
+
+    @Enumerated(EnumType.STRING)
+    private BloodGroupType bloodGroup;
 }
