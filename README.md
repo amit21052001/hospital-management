db07cd3572890c3d6319df6c675cf6205cc01cdf
amit21052001 <amsolanki033@gmail.com>
2026-01-16 18:39:46 +0530

    Added JPQL scripts and DTOs

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Spring Boot Application for Hospital Management
 [![Java](https://img.shields.io/badge/Java-25-orange?logo=java)](https://www.oracle.com/java/)
 [![Spring Boot](https://img.shields.io/badge/Spring%20Boot-3.2.0-brightgreen?logo=springboot)](https://spring.io/projects/spring-boot)
 [![Spring Data JPA](https://img.shields.io/badge/Spring%20Data%20JPA-Enabled-success?logo=spring)](https://spring.io/projects/spring-data-jpa)
+![Release](https://img.shields.io/github/v/release/amit21052001/hospital-management)
--- src/main/java/com/example/hospitalManagement/Repository/PatientRepo.java
+++ src/main/java/com/example/hospitalManagement/Repository/PatientRepo.java
@@ -1,11 +1,39 @@
 package com.example.hospitalManagement.Repository;
 
+import com.example.hospitalManagement.DTO.BloodGroupCountDTO;
 import com.example.hospitalManagement.Entity.Patient;
+import com.example.hospitalManagement.Enum.BloodGroupType;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 
 public interface PatientRepo extends JpaRepository<Patient, Long> {
     Patient findByName(String name);
     List<Patient> findByEmailOrName(String email, String name);
+
+    @Query("select p from Patient p where p.bloodGroup = ?1")
+    List<Patient> findByBloodGroup(@Param("bloodGroup") BloodGroupType bloodGroup);
+
+    @Query("select  p from Patient p where p.birthDate > :birthDate")
+    List<Patient> findByBornAfterDate(@Param("birthDate") LocalDate birthDate);
+
+    @Query("select p.bloodGroup, count(p) from Patient p group by p.bloodGroup")
+    List<Object[]> countOfEachBloodGroupType();
+
+    @Query("select new com.example.hospitalManagement.DTO.BloodGroupCountDTO(p.bloodGroup, count(p)) from Patient p group by p.bloodGroup")
+    List<BloodGroupCountDTO> countOfEachBloodGroupTypeUsingProjection();
+
+    @Query(value = "select * from Patient", nativeQuery = true)
+    List<Patient> findAllPatients();
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Patient p SET p.name = :name where p.id = :id")
+    int updateNameWithId(@Param("name") String name, @Param("id") Long id);
 }
